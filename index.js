598b99591d0e9aaeddbd69f5585fdd42e40e06ec
GaziMaksudur <gazimaksudur2@gmail.com>
2024-09-13 00:37:52 +0600

    jwt issue resolved

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+    ],
+    credentials: true,
+}));
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
@@ -15,6 +23,48 @@ app.get('/', (req, res) => {
 
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.oknyghy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = "mongodb://localhost:27017";
+
+
+const verifyToken = (req, res, next)=>{
+    const token = req.cookies.token;
+    // console.log("token in the middleware ", token);
+    if(!token){
+        req.status(401).send({message: "Unauthorized Access!!"});
+    }else{
+        jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded)=>{
+            if(err){
+                res.status(401).send({message: "Unauthorized Access!!"});
+            }
+            req.user = decoded;
+        })
+    }
+    next();
+}
+
+const logger = (req, res, next)=> {
+    console.log('logged from middleware : ', req.url, req.method);
+    next();
+}
+
+// require('crypto').randomBytes(64).toString('hex') generate secret access key
+app.post('/jwt', (req, res)=>{
+    const user = req.body;
+    const token = jwt.sign({user},process.env.SECRET_ACCESS_TOKEN,{expiresIn: '1h'});
+    res
+    .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+    })
+    .send({success: true});
+});
+
+app.post('/logout', (req, res)=>{
+    const user = req.body;
+    res
+    .clearCookie('token',{maxAge: 0})
+    .send({success: true});
+})
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -48,13 +98,13 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/user', async(req, res)=>{
+        app.get('/user', verifyToken, async(req, res)=>{
             // const id = req.query.id;
             let filter = null;
             if(req?.query?.id){
                 filter = {_id: new ObjectId(req?.query?.id)}
             }else if(req?.query?.email){
-                filter = {mail: req.query.email}
+                filter = {email: req.query.email}
             }else if(req?.query?.username){
                 filter = {username: req.query.username};
             }else{
